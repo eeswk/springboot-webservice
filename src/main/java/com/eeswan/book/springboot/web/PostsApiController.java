7ae9a3066365bd179b348a71c3d93b3fde36fbbf
themorning <themorning84@gmail.com>
2019-12-30 18:30:16 +0900

    수정, 삭제 기능 추가

--- src/main/java/com/eeswan/book/springboot/web/PostsApiController.java
+++ src/main/java/com/eeswan/book/springboot/web/PostsApiController.java
@@ -28,6 +28,16 @@ public class PostsApiController {
     }
 
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+
+
+
+
 
 
 
